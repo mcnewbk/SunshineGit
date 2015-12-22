4fc78e8ec175a8029b52342aba578af6863daad5
brandon <brandon@gatewayapps.com>
2015-12-22 17:15:06 -0600

    Added some lesson additions

--- app/src/main/java/com/gatewayapps/sunshinegit/MainActivity.java
+++ app/src/main/java/com/gatewayapps/sunshinegit/MainActivity.java
@@ -74,6 +74,29 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        if(id == R.id.action_map){
+            openPreferredLocationInMap();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap(){
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+
+        Uri geolocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geolocation);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        } else{
+            Log.d("Sunshine", "Error opening map intent");
+        }
     }
 }
